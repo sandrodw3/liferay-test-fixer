a37cf96fb6e433187c5b4942798ff21c0e325dbf
Sandro Chinea Lorenzo <sandrochinea@gmail.com>
2026-04-27 12:14:23 +0100

    Add test result builder

---
+++ src/lib/get-test-result.ts
@@ -0,0 +1,37 @@
+import { parseHistory } from '@/lib/test-history'
+
+import { TestResult } from '@/types/test-result'
+import { Case, CaseResult, CaseType, History } from '@/types/testray'
+
+export function getTestResult({
+	caseResult,
+	testCase,
+	history,
+	caseTypes,
+}: {
+	caseResult: CaseResult
+	testCase: Case
+	history: History | null
+	caseTypes: CaseType[]
+}): TestResult {
+	const type = getType(testCase, caseTypes)
+
+	return {
+		name: testCase.name,
+		errors: caseResult.errors,
+		history: history && parseHistory(history),
+		type,
+	}
+}
+
+function getType(testCase: Case, types: CaseType[]): TestResult['type'] {
+	if (testCase.name.includes('PortalLogAssertor')) {
+		return 'Java Log Assertor'
+	}
+
+	const type = types.find(
+		({ id }) => id === testCase.r_caseTypeToCases_c_caseTypeId
+	)
+
+	return type!.name
+}
